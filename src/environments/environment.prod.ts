b203c7291c93d83234ee3d1711d59688a4044776
Qarun-Qadir-Bissoondial <qarun80@hotmail.com>
2022-02-28 02:50:57 -0400

    chore: install libs
    - installs Angular Material
    - installs AngularFire
    - removes default front-end app component code

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'skillfill-assessment',
+    appId: '1:524902735149:web:d2161e1038a62aa9a6c7a4',
+    databaseURL: 'https://skillfill-assessment-default-rtdb.firebaseio.com',
+    storageBucket: 'skillfill-assessment.appspot.com',
+    apiKey: 'AIzaSyBL88Y3F_mU-uMklGAJuiXPTtcKu42Ttrg',
+    authDomain: 'skillfill-assessment.firebaseapp.com',
+    messagingSenderId: '524902735149',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'skillfill-assessment',
+    appId: '1:524902735149:web:d2161e1038a62aa9a6c7a4',
+    databaseURL: 'https://skillfill-assessment-default-rtdb.firebaseio.com',
+    storageBucket: 'skillfill-assessment.appspot.com',
+    apiKey: 'AIzaSyBL88Y3F_mU-uMklGAJuiXPTtcKu42Ttrg',
+    authDomain: 'skillfill-assessment.firebaseapp.com',
+    messagingSenderId: '524902735149',
+  },
   production: false
 };
 
