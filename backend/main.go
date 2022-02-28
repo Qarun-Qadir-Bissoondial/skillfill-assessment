db2f60a54c1990535f9ac2463e34e2413c53c693
Qarun-Qadir-Bissoondial <qarun80@hotmail.com>
2022-02-28 01:20:35 -0400

    feat: add endpoints
    - adds endpoint for fetching a quiz
    - adds endpoint for fetching a single question
    - adds endpoint for submitting an answer to a question on a quiz

--- backend/main.go
+++ backend/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	firebase "firebase.google.com/go"
@@ -10,12 +11,25 @@ import (
 )
 
 type Quiz struct {
-	id          string   `json:"id"`
-	name        string   `json:"name"`
-	description string   `json:"description"`
-	questions   []string `json:"questions"`
-	timeLimit   int      `json:"timeLimit"`
-	score       int      `json:"score"`
+	Id          string   `json:"id"`
+	Name        string   `json:"name"`
+	Description string   `json:"description"`
+	Questions   []string `json:"questions"`
+	TimeLimit   int      `json:"timeLimit"`
+	Score       int      `json:"score,omitempty"`
+}
+
+type Question struct {
+	Id              string   `json:"id"`
+	Body            string   `json:"body"`
+	CorrectAnswer   []int    `json:"correctAnswer"`
+	PossibleAnswers []string `json:"possibleAnswers"`
+}
+
+type SubmittedAnswer struct {
+	QuizID     string `json:"quiz"`
+	QuestionID string `json:"question"`
+	Submitted  int    `json:"submittedAnswer"`
 }
 
 func main() {
@@ -35,13 +49,35 @@ func main() {
 	}
 
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		var quiz Quiz
-
+	r.GET("/quiz", func(c *gin.Context) {
+		quiz := Quiz{}
 		quizID := c.Query("quizID")
-		dbClient.NewRef("/quiz/"+quizID).Get(context.Background(), quiz)
-		c.JSON(200, gin.H{
-			"message": quiz,
+		dbClient.NewRef("/quiz/"+quizID).Get(context.Background(), &quiz)
+		c.JSON(200, quiz)
+	})
+
+	r.GET("/question", func(c *gin.Context) {
+		question := Question{}
+		questionID := c.Query("questionID")
+		dbClient.NewRef("/questions/"+questionID).Get(context.Background(), &question)
+		log.Println(question)
+		c.JSON(200, question)
+	})
+
+	r.PUT("/submit-question", func(c *gin.Context) {
+		s := SubmittedAnswer{}
+		c.BindJSON(&s)
+		b := map[string]int{}
+		b[s.QuestionID] = s.Submitted
+		log.Println(b)
+		err := dbClient.NewRef(fmt.Sprintf("/submitted/%s/", s.QuizID)).Set(context.Background(), b)
+		if err != nil {
+			c.JSON(400, map[string]string{
+				"message": "check your params",
+			})
+		}
+		c.JSON(200, map[string]string{
+			"message": "okay",
 		})
 	})
 
